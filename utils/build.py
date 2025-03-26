96c0742df5f549da1ac96afcb04f272f767e3d3d
esamper <chaconyadixa123@gmail.com>
2025-03-26 11:49:30 -0400

    Agragando captura de excepciones en utils/

--- utils/build.py
+++ utils/build.py
@@ -2,5 +2,13 @@ import os
 import subprocess
 
 def create_exe():
-    if not os.path.exists("dist") or not os.path.exists("build"):
-        subprocess.run("pyinstaller --name GameTittan --onefile --windowed --icon=tittan.ico main.py")
+    try:
+        if not os.path.exists("dist") or not os.path.exists("build"):
+            subprocess.run(
+                "pyinstaller --name GameTittan --onefile --windowed --icon=tittan.ico main.py",
+                check=True
+            )
+    except subprocess.CalledProcessError as e:
+        print(f"Error al ejecutar PyInstaller: {e}")
+    except Exception as e:
+        print(f"Ocurrió un error inesperado: {e}")
--- utils/colision.py
+++ utils/colision.py
@@ -1,7 +1,15 @@
 from pyray import check_collision_recs, check_collision_circle_rec
 
 def colision_rect(rect1, rect2):
-    return check_collision_recs(rect1, rect2)
+    try:
+        return check_collision_recs(rect1, rect2)
+    except Exception as e:
+        print(f"Error en colision_rect: {e}")
+        return False
 
 def colision_rect_circle(vector, radius, player):
-    return check_collision_circle_rec(vector, radius, player)
+    try:
+        return check_collision_circle_rec(vector, radius, player)
+    except Exception as e:
+        print(f"Error en colision_rect_circle: {e}")
+        return False
