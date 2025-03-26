f749d3d81524d91725d7109c99fea6fa21f62573
esamper <chaconyadixa123@gmail.com>
2025-03-26 12:48:41 -0400

    Agregando descompresion

--- README.md
+++ README.md
@@ -14,6 +14,18 @@ Game Titan es un emocionante juego de estilo pixel art en el que el objetivo pri
 
 No necesitas ejecutar el código fuente ni instalar dependencias. Simplemente descarga el archivo `dist.zip` desde la carpeta `dist` del repositorio, descomprímelo y haz doble clic en el archivo `.exe` para comenzar a jugar.
 
+**Nota**: Si encuentras un error al descomprimir el archivo `dist.zip`, puede deberse a que `shutil` utiliza un método de compresión que no es compatible con algunos descompresores. En este caso, sigue estos pasos:
+
+1. Descarga el archivo `compress.py` desde la carpeta `utils` del repositorio.
+2. Coloca el archivo `compress.py` en la misma ruta donde se encuentra el archivo `dist.zip`.
+3. Ejecuta el script para descomprimir correctamente el archivo:
+
+    ```bash
+    python compress.py
+    ```
+
+Esto generará los archivos descomprimidos en la misma carpeta.
+
 ### Opción 2: Ejecutar desde el Código Fuente
 
 Si prefieres explorar el código o realizar modificaciones, sigue estos pasos:
--- utils/compress.py
+++ utils/compress.py
@@ -18,3 +18,23 @@ def create_compressed_directory(directory_path):
         print(f"Error: No tienes permisos para acceder al directorio '{directory_path}'.")
     except Exception as e:
         print(f"Ha ocurrido un error inesperado: {e}")
+
+def descrompress_directory(zip_file_path):
+    try:
+        if not os.path.isfile(zip_file_path):
+            print(f"Error: '{zip_file_path}' no es un archivo.")
+            return
+        
+        output_directory = zip_file_path.replace('.zip', '')
+        shutil.unpack_archive(zip_file_path, output_directory)
+        
+        print(f"Directorio descomprimido exitosamente en: {output_directory}")
+    except FileNotFoundError:
+        print(f"Error: El archivo '{zip_file_path}' no existe.")
+    except PermissionError:
+        print(f"Error: No tienes permisos para acceder al archivo '{zip_file_path}'.")
+    except Exception as e:
+        print(f"Ha ocurrido un error inesperado: {e}")
+
+if __name__ == '__main__':
+    if os.path.exists('dist.zip'): descrompress_directory('dist.zip')
