d8096d7147ace66c1637907b9bd49f1677948423
esamper <chaconyadixa123@gmail.com>
2025-03-26 12:06:31 -0400

    Creando compresor para el ejecutable

--- utils/build.py
+++ utils/build.py
@@ -1,6 +1,13 @@
 import os
 import subprocess
 
+from utils.compress import create_compressed_directory
+
+
+def comprimir_exe():
+    if os.path.exists("dist/GameTittan.exe"):
+        create_compressed_directory("dist")
+        
 def create_exe():
     try:
         if not os.path.exists("dist") or not os.path.exists("build"):
@@ -8,6 +15,9 @@ def create_exe():
                 "pyinstaller --name GameTittan --onefile --windowed --icon=tittan.ico main.py",
                 check=True
             )
+            comprimir_exe()
+        else:
+            print("Ya existe el directorio dist o build")
     except subprocess.CalledProcessError as e:
         print(f"Error al ejecutar PyInstaller: {e}")
     except Exception as e:
---
+++ utils/compress.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+def create_compressed_directory(directory_path):
+    try:
+        if not os.path.isdir(directory_path):
+            print(f"Error: '{directory_path}' no es un directorio.")
+            return
+        output_filename = directory_path.rstrip(os.sep) + '.zip'
+        shutil.make_archive(directory_path, 'zip', directory_path)
+        if os.path.exists(output_filename): shutil.rmtree(directory_path)
+        print(f"Directorio comprimido exitosamente en: {output_filename}")
+    except FileNotFoundError:
+        print(f"Error: El directorio '{directory_path}' no existe.")
+    except PermissionError:
+        print(f"Error: No tienes permisos para acceder al directorio '{directory_path}'.")
+    except Exception as e:
+        print(f"Ha ocurrido un error inesperado: {e}")
